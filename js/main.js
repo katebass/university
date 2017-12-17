6c06b87faef4f69aec1038230a50f278a648c8e2
kusamops <katy.bas@mail.ru>
2017-12-17 08:49:03 +0200

    changed nav style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-var nav = document.getElementsByClassName('navigation')[0];
+var navBackground = document.getElementsByClassName('navigation')[0],
+	fontColor = document.getElementsByClassName('nav-text'),
+	activeEl = document.getElementsByClassName('active')[0];
+
+Array.prototype.forEach.call(fontColor, function(el) {
+  		el.style.color = "white";
+} );
 
 window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if(scrolled >= 100) nav.classList.remove('visibility-no');
-  else nav.classList.add('visibility-no');
+  if(scrolled <= 100) {
+  	navBackground.classList.remove('nav-white');
+  	Array.prototype.forEach.call(fontColor, function(el) {
+  		el.style.color = "white";
+  	} );
+  	activeEl.firstChild.style.backgroundColor = "pink";
+  }
+  else {
+  	navBackground.classList.add('nav-white');
+  	Array.prototype.forEach.call(fontColor, function(el) {
+  		el.style.color = "rgb(255, 121, 77)";
+  	} );
+  	activeEl.firstChild.style.backgroundColor = "rgb(184, 184, 148)";
+  }
   console.log(scrolled + 'px');
 }
 
